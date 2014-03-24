4ce4674c149effab26b6888910e85f9ebda55a40
App Academy Student <student@appacademy.io>
2014-03-24 16:51:48 -0400

    adds finding methods.

--- we-scream.rb
+++ we-scream.rb
@@ -1,25 +1,8 @@
-
-
-
-#https://maps.googleapis.com/maps/api/place/nearbysearch/output?parameters
-
-
 require 'rest-client'
 require 'json'
 require 'addressable/uri'
 require_relative 'secret.rb'
-
-
-
-# GET /search?q=cat+hats HTTP/1.1
-# Host: bing.com
-# Content-type:application/json
-#https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&types=food&name=harbour&sensor=false&key=AddYourOwnKeyHere
-
-#http://maps.googleapis.com/maps/api/geocode/output?parameters
-#https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&sensor=true_or_false&key=API_KEY
-
-
+require 'nokogiri'
 
 def my_location(address)
   Addressable::URI.new(
@@ -34,7 +17,17 @@ def my_location(address)
   ).to_s
 end
 
-#https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&types=food&name=harbour&sensor=false&key=AddYourOwnKeyHere
+
+def get_position(address = "770 Broadway New York NY")
+  make_url = my_location(address)
+  response = RestClient.get(make_url)
+  location = JSON.parse(response)
+
+  x = location["results"][0]["geometry"]["location"]["lat"]
+  y = location["results"][0]["geometry"]["location"]["lng"]
+
+  [x,y].join(',')
+end
 
 
 def my_ice_cream_search(arr)
@@ -44,30 +37,49 @@ def my_ice_cream_search(arr)
     :path => "maps/api/place/nearbysearch/json",
     :query_values => {
       :location => arr,
-      :radius => "test",
       :rankby => :distance,
-      :types => "food",
+      #:types => "food",
       :keyword => "ice+cream",
       :sensor => false,
       :key => GOOGLE_KEY
     }
   ).to_s
-
-end
-
-def get_position(address = "770 Broadway New York NY")
-  make_url = my_location(address)
-
-  response = RestClient.get(make_url)
-
-  location = JSON.parse(response)
-
-  x = location["results"][0]["geometry"]["location"]["lat"]
-  y = location["results"][0]["geometry"]["location"]["lng"]
-
-  [x,y]
 end
 
 
+
+def get_ice_create(search_results)
+  response = RestClient.get(search_results)
+  options = JSON.parse(response)
+  names = []
+  x = 0
+  options["results"].each do |shop|
+    names << shop["name"]
+  end
+  names
+end
+
+def pull_coordinates(number, search_results)
+  response = RestClient.get(search_results)
+  options = JSON.parse(response)
+  coord = options["results"][number]["geometry"]["location"]
+  lat = coord["lat"]
+  lng = coord["lng"]
+  result = "#{lat}, #{lng}"
+end
+
+def meow_directions(there, here = get_position)
+ # here is already done - from get_pos
+  Addressable::URI.new(
+    :scheme => "https",
+    :host => "maps.googleapis.com",
+    :path => "maps/api/directions/json",
+    :query_values => {
+      :origin => here,
+      :destination => there,
+      :sensor => false
+    }
+  ).to_s
+end
 
 
